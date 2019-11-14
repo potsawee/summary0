06e8ea5ea69fcd3d741decc0d79210ef068d61cd
Potsawee Manakul <m.potsawee@gmail.com>
2019-11-14 17:38:48 +0000

    process abstractive task data

--- data2.py
+++ data2.py
@@ -9,6 +9,7 @@ from transformers import BertTokenizer
 
 CLS_TOKEN = '[CLS]'
 SEP_TOKEN = '[SEP]'
+MASK_TOKEN = '[MASK]'
 EOS_TOKENS = ['.', '?', '!']
 
 
@@ -22,12 +23,18 @@ class ProcessedDocument(object):
         self.token_type_ids = token_type_ids
         self.cls_pos = cls_pos
 
+class ProcessedSummary(object):
+    def __init__(self, encoded_abstract, length):
+        self.encoded_abstract = encoded_abstract
+        self.length = length
+
 class CNNDMloader(object):
-    def __init__(self, max_sent_length):
+    def __init__(self, max_sent_length, max_summary_length):
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
         self.max_sent_length = max_sent_length
+        self.max_summary_length = max_summary_length
 
-    def process_data(self, filepaths, data_type):
+    def process_data_article(self, filepaths, data_type):
         if data_type == 'train':
             documents = load_data_pickle(filepaths['train'])
         elif data_type == 'val':
@@ -92,6 +99,44 @@ class CNNDMloader(object):
         with open("lib/model_data/{}-{}.dat.nltk.pk.bin".format(data_type, self.max_sent_length), "wb") as f:
             pickle.dump(processed_documents, f)
 
+    def process_data_abstract(self, filepaths, data_type):
+        # this method mimics process_data_article
+        if data_type == 'train':
+            documents = load_data_pickle(filepaths['train'])
+        elif data_type == 'val':
+            documents = load_data_pickle(filepaths['val'])
+        elif data_type == 'test':
+            documents = load_data_pickle(filepaths['test'])
+        else:
+            raise Exception("train/val/test only")
+
+        processed_summaries = [None] * len(documents)
+
+        for idx, document in tqdm(enumerate(documents)):
+            abstract = document[1].decode('utf-8')
+            # note that the abstract is in this format:
+            # <s> sent1 </s> <s> sent2 </s> <s> sent3 </s>
+            # it will be processed into:
+            # [CLS] sent1 [SEP] sent2 [SEP] sent3 [MASK]
+            abstract = abstract.replace('</s> <s>', SEP_TOKEN)
+            abstract = abstract.replace('<s>', CLS_TOKEN).replace('</s>', MASK_TOKEN)
+
+            tokenized_abstract = self.tokenizer.tokenize(abstract)
+
+            # If the sequence is too long, truncate it! currently should be 96
+            if len(tokenized_abstract) > self.max_summary_length:
+                tokenized_abstract = tokenized_abstract[:self.max_summary_length]
+
+            encoded_abstract = [self.tokenizer.convert_tokens_to_ids(MASK_TOKEN)] * self.max_summary_length
+
+            for i, tok in enumerate(tokenized_abstract):
+                encoded_abstract[i] = self.tokenizer.convert_tokens_to_ids(tok)
+
+            length = len(tokenized_abstract)
+            processed_summaries[idx] = ProcessedSummary(encoded_abstract, length)
+
+        with open("lib/model_data/abstract.{}-{}.pk.bin".format(data_type, self.max_summary_length), "wb") as f:
+            pickle.dump(processed_summaries, f)
 
 def load_data_pickle(path):
     """
@@ -152,20 +197,14 @@ def load_extractive_labels(data_type, max_num_sentences):
 
     return target_positions
 
-def sequence_mask(sequence_length, max_len=None):
-    if max_len is None:
-        max_len = sequence_length.data.max()
-    batch_size = sequence_length.size(0)
-    seq_range = torch.range(0, max_len - 1).long()
-    seq_range_expand = seq_range.unsqueeze(0).expand(batch_size, max_len)
-
 def exp():
     filepaths = {}
     filepaths['train'] = '/home/alta/summary/pm574/data/cnn_dm/finished_files_pm574/train.pk.bin'
     filepaths['val'] = '/home/alta/summary/pm574/data/cnn_dm/finished_files_pm574/val.pk.bin'
     filepaths['test'] = '/home/alta/summary/pm574/data/cnn_dm/finished_files_pm574/test.pk.bin'
-    cnndmloader = CNNDMloader(max_sent_length=512)
-    cnndmloader.process_data(filepaths, 'train')
+    cnndmloader = CNNDMloader(max_sent_length=512, max_summary_length=96)
+    # cnndmloader.process_data_article(filepaths, 'train')
+    cnndmloader.process_data_abstract(filepaths, 'train')
     return
 
 
@@ -174,14 +213,19 @@ def cleanup_work():
     # this work aims to remove them!
     path_data        = "lib/model_data/train-512.dat.nltk.pk.bin"
     path_target      = "lib/model_data/target.train-32.pk.bin"
+    path_abstract    = "lib/model_data/abstract.train-96.pk.bin"
     with open(path_data, "rb") as f: data = pickle.load(f)
     with open(path_target, "rb") as f: target_pos = pickle.load(f)
+    with open(path_abstract, "rb") as f: summaries = pickle.load(f)
+
     assert len(data) == len(target_pos), "len(data) != len(target_pos)"
+    assert len(data) == len(summaries), "len(data) != len(summaries)"
 
     num_data = len(data)
     count    = 0
     clean_data = []
     clean_target_pos = []
+    clean_summaries = []
     for i, doc in enumerate(data):
         # encoded_articles[i]   = doc.encoded_article
         # attention_masks[i]    = doc.attention_mask
@@ -193,12 +237,15 @@ def cleanup_work():
         else:
             clean_data.append(doc)
             clean_target_pos.append(target_pos[i])
+            clean_summaries.append(summaries[i])
     print("remove {} files out of {} in original".format(count, num_data))
 
-    with open("lib/model_data/trainx-512.dat.nltk.pk.bin", "wb") as f:
-        pickle.dump(clean_data, f)
-    with open("lib/model_data/target.trainx-32.pk.bin", "wb") as f:
-        pickle.dump(clean_target_pos, f)
+    # with open("lib/model_data/trainx-512.dat.nltk.pk.bin", "wb") as f:
+    #     pickle.dump(clean_data, f)
+    # with open("lib/model_data/target.trainx-32.pk.bin", "wb") as f:
+    #     pickle.dump(clean_target_pos, f)
+    with open("lib/model_data/abstract.trainx-96.pk.bin", "wb") as f:
+        pickle.dump(clean_summaries, f)
     print("cleanup done & saved")
 
 if __name__ == "__main__":
