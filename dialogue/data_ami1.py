4d0309402ba67eb60f43d6f6f6cb20be1f9bc5ea
Potsawee Manakul <m.potsawee@gmail.com>
2019-11-27 14:08:10 +0000

    initial work on dialog summarisation

---
+++ dialogue/data_ami1.py
@@ -0,0 +1,101 @@
+DATA_DIR   = '/home/alta/summary/pm574/summariser0/lib/ami_goochen/'
+CLS_TOKEN  = "[CLS]"
+SEP_TOKEN  = "[SEP]"
+MASK_TOKEN = "[MASK]"
+
+from transformers import BertTokenizer
+bert_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+
+class ProcessedDocument(object):
+    def __init__(self, encoded_article, attention_mask, token_type_ids, cls_pos):
+        self.encoded_article = encoded_article
+        self.attention_mask = attention_mask
+        self.token_type_ids = token_type_ids
+        self.cls_pos = cls_pos
+
+class ProcessedSummary(object):
+    def __init__(self, encoded_abstract, length):
+        self.encoded_abstract = encoded_abstract
+        self.length = length
+
+def load_data(data_type, max_sent_length, max_summary_length):
+    data_dict = read_data(DATA_DIR+'{}/'.format(data_type))
+    num_data  = len(data_dict['in'])
+
+    # ------------------------------------- INPUT ------------------------------------- #
+    processed_documents = [None] * num_data
+    for idx in range(num_data):
+        utterances = data_dict['in'][idx].replace(".","").split("<eos>")[:-1]
+        tokenized_input = []
+        for utterance in utterances:
+            words = [CLS_TOKEN] + bert_tokenizer.tokenize(utterance) + [SEP_TOKEN]
+            tokenized_input += words
+        if len(tokenized_input) > max_sent_length:
+            tokenized_input = tokenized_input[:max_sent_length]
+            print("ID: {} is too long".format(idx))
+
+        encoded_input = [bert_tokenizer.convert_tokens_to_ids('[UNK]')] * max_sent_length
+        token_type_ids = [0] * max_sent_length # 0 and 1 alternate
+        cls_pos = []
+        type_id = 0
+
+        for i, tok in enumerate(tokenized_input):
+            encoded_input[i] = bert_tokenizer.convert_tokens_to_ids(tok)
+            token_type_ids[i] = type_id
+            if tok == CLS_TOKEN:
+                cls_pos.append(i)
+            if tok == SEP_TOKEN:
+                type_id = (type_id + 1) % 2
+
+        # len(tokenized_article) is at most self.max_sent_length
+        attention_mask = ([1]*len(tokenized_input) + [0]*(max_sent_length-len(tokenized_input)))
+
+        processed_documents[idx] = ProcessedDocument(encoded_input, attention_mask, token_type_ids, cls_pos)
+
+    # ----------------------------------- SUMMARY ----------------------------------- #
+    processed_summaries = [None] * num_data
+    for idx in range(num_data):
+        summary  = data_dict['sum'][idx]
+        # note that the abstract is in this format:
+        # <s> sent1 </s> <s> sent2 </s> <s> sent3 </s>
+        # it will be processed into:
+        # [CLS] sent1 [SEP] sent2 [SEP] sent3 [MASK]
+        # but there is only one sentence in this AMI data
+        summary  = "{} {} {}".format(CLS_TOKEN, summary, MASK_TOKEN)
+        tokenized_sum = bert_tokenizer.tokenize(summary)
+
+        # If the sequence is too long, truncate it! currently should be 96
+        if len(tokenized_sum) > max_summary_length:
+            tokenized_sum = tokenized_sum[:max_summary_length]
+
+        encoded_sum = [bert_tokenizer.convert_tokens_to_ids(MASK_TOKEN)] * max_summary_length
+
+        for i, tok in enumerate(tokenized_sum):
+            encoded_sum[i] = bert_tokenizer.convert_tokens_to_ids(tok)
+
+        length = len(tokenized_sum)
+        processed_summaries[idx] = ProcessedSummary(encoded_sum, length)
+
+    return {'in': processed_documents, 'sum': processed_summaries}
+
+def read_data(data_dir):
+    # dialog act
+    da = data_dir + 'da'
+    with open(da, 'r') as f:
+        dialog_acts = f.readlines()
+        dialog_acts = [d.strip() for d in dialog_acts]
+     # summary
+    sum = data_dir + 'sum'
+    with open(sum, 'r') as f:
+        summaries = f.readlines()
+        summaries = [s.strip() for s in summaries]
+        # dialog act
+    inp = data_dir + 'in'
+    with open(inp, 'r') as f:
+        inputs = f.readlines()
+        inputs = [i.strip() for i in inputs]
+
+    assert len(dialog_acts) == len(inputs)
+    assert len(dialog_acts) == len(summaries)
+
+    return {'da': dialog_acts, 'in': inputs, 'sum': summaries}
---
+++ dialogue/decode_ami1.py
@@ -0,0 +1,270 @@
+import os
+import sys
+sys.path.append("/home/alta/summary/pm574/summariser0/")
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import pdb
+import pickle
+import random
+from datetime import datetime
+
+from data_ami1 import load_data
+from models.abstractive import *
+from train_abstractive import shuffle_data2, get_a_batch_abs, adjust_lr_not_improved, adjust_lr2, shift_decoder_target, evaluate2, LabelSmoothingLoss
+
+if   torch.__version__ == '1.1.0': KEYPADMASK_DTYPE = torch.uint8
+elif torch.__version__ == '1.2.0': KEYPADMASK_DTYPE = torch.bool
+else: raise Exception("Torch Version not supoorted")
+
+from transformers import BertTokenizer
+bert_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+
+def train_abstractive_model():
+    print("Start training abstractive model")
+    # ---------------------------------------------------------------------------------- #
+    args = {}
+    args['max_pos_embed'] = 512
+    args['max_num_sentences'] = 32
+    args['update_nbatches'] = 5
+    args['batch_size'] = 6
+    args['num_epochs'] = 50
+    args['val_batch_size'] = 64
+    args['val_stop_training'] = 100
+    args['random_seed'] = 224
+    args['lr_enc'] = 1e-5
+    args['lr_dec'] = 1e-4
+    args['adjust_lr'] = False
+    # ---------------------------------------------------------------------------------- #
+    args['use_gpu'] = True
+    args['model_save_dir'] = "/home/alta/summary/pm574/summariser0/lib/trained_models/"
+    args['model_data_dir'] = "/home/alta/summary/pm574/summariser0/lib/model_data/"
+    args['model_name'] = "DIALAMINOV27A"
+    args['load_model'] = "/home/alta/summary/pm574/summariser0/lib/trained_models/abssum-ANOV21A-ep9-bn40000.pt"
+    # args['load_model'] = None
+    args['best_val_loss'] = 1e+10
+    # ---------------------------------------------------------------------------------- #
+    args['max_summary_length'] = 96
+    args['label_smoothing'] = 0.1
+    # ---------------------------------------------------------------------------------- #
+
+    if args['use_gpu']:
+        if 'X_SGE_CUDA_DEVICE' in os.environ: # to run on CUED stack machine
+            print('running on the stack...')
+            cuda_device = os.environ['X_SGE_CUDA_DEVICE']
+            print('X_SGE_CUDA_DEVICE is set to {}'.format(cuda_device))
+            os.environ['CUDA_VISIBLE_DEVICES'] = cuda_device
+        else:
+            # pdb.set_trace()
+            print('running locally...')
+            os.environ["CUDA_VISIBLE_DEVICES"] = '0' # choose the device (GPU) here
+        device = 'cuda'
+    else:
+        device = 'cpu'
+    print("device = {}".format(device))
+
+    # Get the data
+    print("========== Loading train data ==========")
+    train_data_ = load_data('train', args['max_pos_embed'], args['max_summary_length'])
+    print("========== Loading valid data ==========")
+    val_data_   = load_data('valid', args['max_pos_embed'], args['max_summary_length'])
+
+    # ----------------------- Prepare the data ----------------------- #
+    processed_documents = train_data_['in']
+    processed_summaries = train_data_['sum']
+    N = len(processed_documents)
+    N2 = len(processed_summaries)
+    x1 = [None] * N
+    x2 = [None] * N
+    x3 = [None] * N
+    x4 = [None] * N
+    y1 = [None] * N
+    y2 = [None] * N
+    for j in range(N):
+        x1[j] = processed_documents[j].encoded_article
+        x2[j] = processed_documents[j].attention_mask
+        x3[j] = processed_documents[j].token_type_ids
+        x4[j] = processed_documents[j].cls_pos
+        y1[j] = processed_summaries[j].encoded_abstract
+        y2[j] = processed_summaries[j].length
+    dummy_target_pos = [0] * N
+    train_data = {'encoded_articles': x1, 'attention_masks': x2,
+                  'token_type_ids_arr': x3, 'cls_pos_arr': x4,
+                  'target_pos': dummy_target_pos, 'num_data': N}
+    train_summary = {'encoded_abstracts': y1, 'abs_lengths': y2, 'num_data': N2}
+
+    processed_documents = val_data_['in']
+    processed_summaries = val_data_['sum']
+    N = len(processed_documents)
+    N2 = len(processed_summaries)
+    x1 = [None] * N
+    x2 = [None] * N
+    x3 = [None] * N
+    x4 = [None] * N
+    y1 = [None] * N
+    y2 = [None] * N
+    for j in range(N):
+        x1[j] = processed_documents[j].encoded_article
+        x2[j] = processed_documents[j].attention_mask
+        x3[j] = processed_documents[j].token_type_ids
+        x4[j] = processed_documents[j].cls_pos
+        y1[j] = processed_summaries[j].encoded_abstract
+        y2[j] = processed_summaries[j].length
+    dummy_target_pos = [0] * N
+    val_data = {'encoded_articles': x1, 'attention_masks': x2,
+                  'token_type_ids_arr': x3, 'cls_pos_arr': x4,
+                   'target_pos': dummy_target_pos, 'num_data': N}
+    val_summary = {'encoded_abstracts': y1, 'abs_lengths': y2, 'num_data': N2}
+    # ------------------------------------------------------------------- #
+
+    # random seed
+    random.seed(args['random_seed'])
+
+    abs_sum = AbstractiveSummariser(args, device=device)
+    print(abs_sum)
+
+    # Load model if specified (path to pytorch .pt)
+    pretrained_dict = torch.load(args['load_model'])
+    abs_sum.load_state_dict(pretrained_dict)
+    abs_sum.train()
+    print("Loaded model from {}".format(args['load_model']))
+
+    # Hyperparameters
+    BATCH_SIZE = args['batch_size']
+    NUM_EPOCHS = args['num_epochs']
+    VAL_BATCH_SIZE = args['val_batch_size']
+    VAL_STOP_TRAINING = args['val_stop_training']
+
+    vocab_size = abs_sum.decoder.linear_decoder.out_features
+
+    if args['label_smoothing'] > 0.0:
+        criterion = LabelSmoothingLoss(num_classes=vocab_size,
+                        smoothing=args['label_smoothing'], reduction='none')
+    else:
+        criterion = nn.NLLLoss(reduction='none')
+
+    # we use two separate optimisers (encoder & decoder)
+    optimizer_enc = optim.Adam(abs_sum.encoder.parameters(),lr=args['lr_enc'],betas=(0.9,0.999),eps=1e-08,weight_decay=0)
+    optimizer_dec = optim.Adam(abs_sum.decoder.parameters(),lr=args['lr_dec'],betas=(0.9,0.999),eps=1e-08,weight_decay=0)
+    optimizer_enc.zero_grad()
+    optimizer_dec.zero_grad()
+
+    # validation losses
+    best_val_loss = args['best_val_loss']
+    best_epoch = 0
+    best_bn = 0
+    stop_counter = 0
+
+    for epoch in range(NUM_EPOCHS):
+        print("======================= Training epoch {} =======================".format(epoch))
+        num_batches = int(train_data['num_data']/BATCH_SIZE) + 1
+        print("num_batches = {}".format(num_batches))
+
+        # Random shuffle the training data
+        train_data, train_summary = shuffle_data2(train_data, train_summary)
+
+        idx = 0
+
+        for bn in range(num_batches):
+            # adjust the learning rate of the optimizer
+            if args['adjust_lr']:
+                adjust_lr2(optimizer_enc, optimizer_dec,
+                        epoch, epoch_size=num_batches, bn=bn,
+                        warmup_enc=20000, warmup_dec=10000)
+
+            # check if it is the last batch
+            if bn == (num_batches - 1):
+                last_batch = True
+                continue # unepxected error --- num_data % batch_size = 0
+            else: last_batch = False
+
+            batch = get_a_batch_abs(train_data['encoded_articles'], train_data['attention_masks'],
+                                train_data['token_type_ids_arr'], train_data['cls_pos_arr'],
+                                train_data['target_pos'], args['max_num_sentences'],
+                                train_summary['encoded_abstracts'], train_summary['abs_lengths'],
+                                args['max_summary_length'],
+                                idx, BATCH_SIZE, last_batch, device)
+
+            # decoder target
+            decoder_target, decoder_mask = shift_decoder_target(batch['decoder'])
+            decoder_target = decoder_target.view(-1)
+            decoder_mask = decoder_mask.view(-1)
+
+            # forward + backward
+            decoder_output = abs_sum(batch)
+
+            loss = criterion(decoder_output.view(-1, vocab_size), decoder_target)
+            loss = (loss * decoder_mask).sum() /  decoder_mask.sum()
+            loss.backward()
+
+            idx += BATCH_SIZE
+
+            if bn % args['update_nbatches'] == 0:
+                # gradient_clipping
+                max_norm = 1.0
+                nn.utils.clip_grad_norm_(abs_sum.parameters(), max_norm)
+                # update the gradients
+                optimizer_enc.step()
+                optimizer_dec.step()
+                optimizer_enc.zero_grad()
+                optimizer_dec.zero_grad()
+
+            if bn % 50 == 0:
+                _tgt  = bert_tokenizer.decode(batch['decoder'][0][0].cpu().numpy())
+                _pred = bert_tokenizer.decode(torch.argmax(decoder_output[0], dim=-1).cpu().numpy())
+                _i = _tgt.find('[MASK]')
+                print("TARGET:", _tgt[:_i])
+                print("PREDIC:", _pred[:_i])
+
+            if bn % 50 == 0:
+                print("[{}] batch number {}/{}: loss = {}".format(str(datetime.now()), bn, num_batches, loss))
+                sys.stdout.flush()
+
+            if bn == 0: # e.g. eval every epoch
+                # ---------------- Evaluate the model on validation data ---------------- #
+                print("Evaluating the model at epoch {} step {}".format(epoch, bn))
+                print("learning_rate_encoder = {}".format(optimizer_enc.param_groups[0]['lr']))
+                print("learning_rate_decoder = {}".format(optimizer_dec.param_groups[0]['lr']))
+                abs_sum.eval() # switch to evaluation mode
+                with torch.no_grad():
+                    avg_val_loss = evaluate2(abs_sum, val_data, val_summary, VAL_BATCH_SIZE, vocab_size, args, device)
+                print("avg_val_loss_per_token = {}".format(avg_val_loss))
+                abs_sum.train() # switch to training mode
+                # ------------------- Save the model OR Stop training ------------------- #
+                if avg_val_loss < best_val_loss:
+                    stop_counter = 0
+                    best_val_loss = avg_val_loss
+                    best_epoch = epoch
+                    best_bn = bn
+                    savepath = args['model_save_dir']+"abssum-{}-ep{}-bn{}.pt".format(args['model_name'],epoch,bn)
+                    torch.save(abs_sum.state_dict(), savepath)
+                    print("Model improved & saved at {}".format(savepath))
+                else:
+                    print("Model not improved #{}".format(stop_counter))
+                    if stop_counter < VAL_STOP_TRAINING:
+                        # load the previous model
+                        # latest_model = args['model_save_dir']+"abssum-{}-ep{}-bn{}.pt".format(args['model_name'],best_epoch,best_bn)
+                        # abs_sum.load_state_dict(torch.load(latest_model))
+                        # abs_sum.train()
+                        # print("Restored model from {}".format(latest_model))
+                        # adjust_lr_not_improved(optimizer_enc, optimizer_dec, factor=0.5)
+                        stop_counter += 1
+
+                    else:
+                        print("Model has not improved for {} times! Stop training.".format(VAL_STOP_TRAINING))
+                        return
+
+    print("End of training abstractive model")
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        # ----- EVALUATION ----- #
+        if sys.argv[1] == 'eval':
+            path_to_model = sys.argv[2]
+            evaluate_model(path_to_model)
+        else:
+            raise Exception("do you want to do evaluation?")
+    elif len(sys.argv) == 1:
+        # ------ TRAINING ------ #
+        train_abstractive_model()
